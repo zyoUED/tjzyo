34d69db346f3c6ae2c803fd14bd34357abad2046
cdll <cd234350150@gmail.com>
2015-12-11 22:32:35 +0800

    fix the listening port at 3000(need root access for ports which are less than 1024 in linux and unix os);

--- App/Conf/config.js
+++ App/Conf/config.js
@@ -1,6 +1,6 @@
 module.exports = {
   //配置项: 配置值
-  port: process.env.PORT||300, //监听的端口
+  port: process.env.PORT||3000, //监听的端口
   db_type: 'mysql', // 数据库类型
   db_host: process.env.IP||'127.0.0.1', // 服务器地址
   db_port: '', // 端口
